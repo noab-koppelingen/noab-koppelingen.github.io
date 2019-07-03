fad29e65d5cac485ed7b6add81a7036e313a0c7b
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-07-03 13:51:44 +0200

    Added referrer check

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,33 @@
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1,c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+jQuery(document).ready(function() {
+  "use strict";
+  if (!getCookie("noab")) {
+    if (window.location.search.indexOf("n=CY5AKFIFviuq8L1SvKR3tCJt0") > -1) {
+      setCookie("noab", "volgende", 365);
+    } else {
+      jQuery("body").hide();
+    }
+  }
+});
 
 jQuery.extend({
   randomColor: function() {
